2024982f2f880ac8ee1fc0fb6f4683b0ba001950
Danny Van den Wouwer <dvdwouwe@peopleware.be>
2026-06-27 15:11:31 +0200

    PPWCODE-50 Replace DummyTest with a new BaseTests abstract class setup.

---
+++ src/I.Utils.Tests/BaseTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+namespace PPWCode.Common.I.Utils.Tests;
+
+[TestFixture]
+[Parallelizable(ParallelScope.Fixtures)]
+public abstract class BaseTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        OnSetup();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        OnTearDown();
+    }
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        OnOneTimeSetup();
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        OnOneTimeTearDown();
+    }
+
+    private static int _nextId = 1000000;
+
+    protected static int NextId
+        => Interlocked.Increment(ref _nextId);
+
+    protected virtual void OnOneTimeTearDown()
+    {
+    }
+
+    protected virtual void OnOneTimeSetup()
+    {
+    }
+
+    protected virtual void OnSetup()
+    {
+    }
+
+    protected virtual void OnTearDown()
+    {
+    }
+}
